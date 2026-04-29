ed3a217faec74b1c79e41d47b0dc4035dbb50622
Makajler <milosz.pawlowicz@zslobez.pl>
2026-04-29 10:31:49 +0200

    new

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from tkinter import *
 from tkinter import ttk
 from typing import List
+import tkinter.font
 root = Tk("TicTacToe","TicTacToe","TicTacToe")
+tkFont = tkinter.font.Font(root,family="Calibri",size=34)
+tkFont2 = tkinter.font.Font(root,family="Calibri",size=17)
+root.configure(background="#0F0F0F")
 frm = Frame(root)
 frm.place(in_=root, anchor="c", relx=.5, rely=.5,y=14)
 player_turn = 0
-players = ["X","O"]
+players = ("X","O")
 WIN = None
-ttk.Label(root, text="Kulko i Kżyrzyk").place(in_=root,anchor="n",relx=.5)
-informacje = ttk.Label(root, text="Kolej \"X\"")
-informacje.place(in_=root,anchor="n",relx=.5,y=16)
+ttk.Label(root, text="Kółko i Krzyżk",foreground="white",background="#0F0F0F", font=tkFont).place(in_=root,anchor="n",relx=.5)
+informacje = ttk.Label(root, text="Kolej \"X\"",foreground="white",background="#0F0F0F", font=tkFont2)
+informacje.place(in_=root,anchor="n",relx=.5,y=52)
 def checkIfWon():
     global Pola, WIN
     for _y in range(3):
@@ -31,7 +35,6 @@ def checkIfWon():
         Lost = False
         for i in range(3):
             Pole = Pola[x+i*3]
-            print(x+i*3)
             if Pole.claimedBy == "" or (a != None and a != Pole.claimedBy):
                 Lost = True
                 break
@@ -65,7 +68,8 @@ def reset():
 class Pole():
     def __init__(self,i):
         self.claimedBy = ""
-        self.button = Button(frm, text="", width=5,command= lambda: self.press(players[player_turn]))
+        self.button = Button(frm, text="", width=4,height=1,command= lambda: self.press(players[player_turn]))
+        self.button["font"] = tkFont
         self.button.grid(column=(i % 3), row=int(i / 3))
     def press(self,claimedby):
         global player_turn
